2c3677a5e76b7b2f7c2485fa8232141d0e0d3d0e
Xavier Salazar <x.salazar1729@gmail.com>
2022-10-15 20:18:05 -0700

    Refactor scraping file

--- index.js
+++ index.js
@@ -1,40 +1,42 @@
 const emojiData = require("emoji-datasource-twitter/emoji_pretty.json");
 const twemoji = require("twemoji");
-const axios = require("axios").default;
-var fs = require("fs");
-
-emojis = [];
-emojiData.forEach((emoji) => {
-  if (emoji.has_img_twitter) {
-    // Create local path for emoji category
-    path = `downloads/${emoji.category}`;
-    fs.mkdirSync(path, { recursive: true }, (err) => {
-      if (err) throw err;
-    });
-
-    // Parse emoji and save URL
-    twemoji.parse(
-      emoji.unified.split("-").map(twemoji.convert.fromCodePoint).join(""),
-      {
-        callback: function (iconId, options) {
-          url = `${options.base}svg/${iconId}.svg`;
-          emojis.push({ url: url, shortName: emoji.short_name, path: path });
-        },
-      }
-    );
-  }
-});
-
-getData();
+const axios = require("axios");
+const fs = require("fs");
 
 async function getData() {
+  var emojis = [];
+
+  emojiData.forEach((emoji) => {
+    if (emoji.has_img_twitter) {
+      // Create local path for emoji category
+      path = `downloads/${emoji.category}`;
+      fs.mkdirSync(path, { recursive: true }, (err) => {
+        if (err) throw err;
+      });
+
+      // Parse emoji and save URL
+      twemoji.parse(
+        emoji.unified.split("-").map(twemoji.convert.fromCodePoint).join(""),
+        {
+          callback: function (iconId, options) {
+            url = `${options.base}svg/${iconId}.svg`;
+            emojis.push({ url: url, shortName: emoji.short_name, path: path });
+          },
+        }
+      );
+    }
+  });
+
   for (let i = 0; i < emojis.length; i++) {
     const emoji = emojis[i];
     try {
+      console.log(`Requesting ${emoji.url}`);
+
       let response = await axios.get(emoji.url, {
         responseType: "stream",
         timeout: 5000,
       });
+
       response.data.pipe(
         fs.createWriteStream(`${emoji.path}/${emoji.shortName}.svg`)
       );
@@ -43,3 +45,5 @@ async function getData() {
     }
   }
 }
+
+getData();
